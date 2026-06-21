350919b920892e2612de6a808c5c32b3a3f7bf98
Nido Maphosa <nidomaphosa86@gmail.com>
2026-06-21 11:44:18 +0200

    conflict resolved

--- CareerHub.Api/obj/Debug/net10.0/CareerHub.Api.AssemblyInfo.cs
+++ CareerHub.Api/obj/Debug/net10.0/CareerHub.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareerHub.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cada3151dd05295a1971fccd92d482b740bbe8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2fef8e3321cebd5c297d1acbe0de36d145cb480")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareerHub.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareerHub.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
